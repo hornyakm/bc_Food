d82adba21d0a15c1975a2e6d8d1a762f2d08ed4a
hornyakm <25149984+hornyakm@users.noreply.github.com>
2018-07-17 09:22:43 +0200

    Add files via upload
    Add deckerization

---
+++ bc_Food_R/plumberize.R
@@ -0,0 +1,27 @@
+
+
+require(plumber)
+require(jsonlite)
+
+custom_json <- function(){
+  function(val, req, res, errorHandler){
+    tryCatch({
+      json <- jsonlite::toJSON(val,auto_unbox=TRUE)
+
+      res$setHeader("Content-Type", "application/json")
+      res$body <- json
+
+      return(res$toResponse())
+    }
+    , 
+    error=function(e){
+     errorHandler(req, res, e)
+   }
+  )
+  }
+}
+
+addSerializer("custom_json",custom_json)
+# Make sure you put the path to your blockchain-node-server.R script
+r <- plumb("/usr/local/lib/R/site-library/plumber/blockchaincore/blockchain-node-server.R")
+r$run(port=8000)
